c9b8157c8281b93e1c64f2f7d6f41b16d8d32d33
AJ Auntor <auntorknocks@gmail.com>
2021-05-27 12:02:28 +0600

    Firebase config and component added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
-     <h1>This is a Header!</h1>
+     
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBkkWU0zX273EXlrdw2gZQ9JI1BzIjlBSc",
+    authDomain: "clone-3e211.firebaseapp.com",
+    projectId: "clone-3e211",
+    storageBucket: "clone-3e211.appspot.com",
+    messagingSenderId: "334327029070",
+    appId: "1:334327029070:web:654ddd02ed2dbbc744d21f",
+    measurementId: "G-YPLKK607S9"
+  };
