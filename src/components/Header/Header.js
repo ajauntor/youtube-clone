2eaa0d12fc9d62e0cf9bf79b2c178f7c39e626f2
AJ Auntor <auntorknocks@gmail.com>
2021-05-27 14:17:21 +0600

    Function added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import './Header.css'
 import logo from './../../Images/logo-yt.png'
+import MicIcon from '@material-ui/icons/Mic';
 import MenuIcon from '@material-ui/icons/Menu';
+import AppsIcon from '@material-ui/icons/Apps';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 const Header = () => {
     return (
-        <div>
+        <div className="header">
             <div className="header-left">
                 <MenuIcon/>
-                <img src={logo} alt="" />
+                <img className="header-logo" src={logo} alt="" />
+            </div>
+            <div className="header-middle">
+                <input type="text" placeholder="Search" />
+                <SearchIcon className="search-icon"/>
+            </div>
+            <div className="header-right">
+                <MicIcon className="header-icons" />
+                <VideoCallIcon className="header-icons"/>
+                <AppsIcon className="header-icons"/>
+                <AccountCircleIcon className="header-icons"/>
+                <NotificationsIcon/>
             </div>
         </div>
     );
