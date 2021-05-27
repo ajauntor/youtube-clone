83c6ec006d4bc77c4813b2dda599f5cfc7adef8a
AJ Auntor <auntorknocks@gmail.com>
2021-05-27 12:10:05 +0600

    Header Component start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Header from './components/Header/Header';
 import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-     
+
+        <Header/>
+        
     </div>
   );
 }
