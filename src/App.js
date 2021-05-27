01e9bbb67d415af0d74eb396608a3a9ab500dd2d
AJ Auntor <auntorknocks@gmail.com>
2021-05-27 12:12:26 +0600

    FTC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Header from './components/Header/Header';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
 
         <Header/>
-        
+
     </div>
   );
 }
